a6d42ac8e342f5baca9b06da87836a962f78d8c3
Jerome Lalunio <josejeromelalunio@gmail.com>
2021-01-06 13:37:35 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import './views/home/home_view.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hello'),
+        ),
+        body: HomeView(),
+      ),
+    );
+  }
+}
