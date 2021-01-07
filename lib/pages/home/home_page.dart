36229a6a83616e3202cbe10ccf65bffd3c50c62f
Jerome Lalunio <josejeromelalunio@gmail.com>
2021-01-07 15:06:49 +0800

    added files that caused bug

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:webtry/routing/route_names.dart';
+import 'package:webtry/routing/router.dart';
+import 'package:webtry/services/navigator_service.dart';
+import 'package:webtry/widgets/locator.dart';
+import 'package:webtry/widgets/navigation_drawer/navigation_drawer.dart';
+import 'package:webtry/widgets/centered_widget.dart';
+import 'package:webtry/widgets/navigation/navigation_bar.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) => Scaffold(
+        drawer: sizingInformation.deviceScreenType == DeviceScreenType.mobile
+            ? NavigationDrawer()
+            : null,
+        backgroundColor: Colors.white,
+        body: CenteredWidget(
+          child: Column(
+            children: [
+              NavigationBar(),
+              Expanded(
+                child: Navigator(
+                  key: locator<NavigationService>().navigatorKey,
+                  onGenerateRoute: generateRoute,
+                  initialRoute: AboutRoute,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
